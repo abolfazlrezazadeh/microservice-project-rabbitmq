8a7bbb72fff5486b433f6a50333c863c998ee376
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-01-20 12:29:05 +0330

    add route for register user

--- auth-service/handler/auth.routes.js
+++ auth-service/handler/auth.routes.js
@@ -1,5 +1,8 @@
+const { productController } = require("../controller/product.controller");
+
 const router = require("express").Router();
 
+router.post("/register" , productController.registerUser)
 
 
 module.exports={
