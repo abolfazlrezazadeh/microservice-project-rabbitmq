a42b10d24118940d3fdd21f528f4201afde8d31a
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-01-20 14:54:38 +0330

    login user in controller side

--- auth-service/controller/auth.controller.js
+++ auth-service/controller/auth.controller.js
@@ -1,7 +1,9 @@
 const { userModel } = require("../model/user");
 const controller = require("./controller");
+const jwt = require("jsonwebtoken")
 const { StatusCodes: httpStatus } = require("http-status-codes");
 const createError = require("http-errors");
+const {SECRET_KEY} = process.env;
 const { validateEmail } = require("./validator/auth.validator");
 class authController {
   async registerUser(req, res, next) {
@@ -27,6 +29,22 @@ class authController {
   }
   async loginUser(req, res, user) {
     try {
+      const {email, password} = req.body;
+      await validateEmail.validateAsync(req.body);
+      const existUser = await userModel.findOne({email},{__v : 0})
+      if(!existUser) throw createError.NotFound("user not found")
+      if(existUser.password !== password) throw createError.BadRequest("password is not correct")
+      delete existUser.password;
+      jwt.sign({email,userId : existUser._id, name :existUser.name},SECRET_KEY,(err,token)=>{
+        if(err) throw createError.InternalServerError("failed, please try again")
+        return res.status(httpStatus.OK).json({
+          statusCode : httpStatus.OK,
+          data : {
+            message : "login successfuly",
+            token 
+          }
+        })
+      })
     } catch (error) {
       next(error);
     }
--- auth-service/controller/validator/auth.validator.js
+++ auth-service/controller/validator/auth.validator.js
@@ -6,7 +6,6 @@ const validateEmail = joi.object({
     .min(3)
     .pattern(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)
     .error(new Error("email in not correct")),
-  name: joi.string().min(3).error(new Error("name must be at least 3 characters")),
   password: joi.string().min(3).error(new Error("password cannot be empty")),
 });
 
