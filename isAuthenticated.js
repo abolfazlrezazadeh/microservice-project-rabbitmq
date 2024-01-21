1f785e0a5c4067f16d7dd625638f5ff66cc14b0e
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-01-21 12:57:23 +0330

    adding is Auth to project

---
+++ isAuthenticated.js
@@ -0,0 +1,31 @@
+const httpStatus = require("http-status");
+const jwt = require("jsonwebtoken");
+
+async function isAuth(req, res, next) {
+  try {
+    const token = req.headers?.["authorization"]?.split(" ")[0] || "";
+    jwt.verify(token, "secretKey", (error, paylod) => {
+      if (err) {
+        return res.status(httpStatus[401]).json({
+          statusCode: httpStatus[401],
+          data: {
+            error: error.message,
+          },
+        });
+      }
+      req.user = paylod;
+      next();
+    });
+  } catch (error) {
+    return res.status(httpStatus[401]).json({
+      statusCode: httpStatus[401],
+      data: {
+        error: error.message,
+      },
+    });
+  }
+}
+
+module.exports={
+    isAuth
+}
