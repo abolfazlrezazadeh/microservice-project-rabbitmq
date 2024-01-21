3962d5baa52f215c02e554b22e8b6cb679a3d5de
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-01-21 10:22:13 +0330

    create controller files

---
+++ product-service/controller/product.controller.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken")
+const { StatusCodes: httpStatus } = require("http-status-codes");
+const createError = require("http-errors");
+
+class productCOntroler {
+    async createProduct(req,res,next){
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+    async createProduct(req,res,next){
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+    async createProduct(req,res,next){
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+}
