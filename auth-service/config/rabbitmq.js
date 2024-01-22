ed14a90042f224ffd9006d941ced4eacc840c00f
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-01-22 21:33:23 +0330

    config rabbitmq file

---
+++ auth-service/config/rabbitmq.js
@@ -0,0 +1,32 @@
+const amqp = require("amqplib");
+let channel;
+const connectToChannel = async () => {
+  try {
+    const connection = await amqp.connect("amqp://localhost:5672");
+    return await connection.createChannel();
+  } catch (error) {
+    return console.log("can not connect to channel in rabbitmq");
+  }
+};
+
+const returnChannel = async () => {
+  if (!channel) {
+    channel = await connectToChannel();
+  }
+  return channel;
+};
+
+const pushToQueue = async (queueName, data) => {
+  try {
+    await channel.assertQueue(queueName);
+    return channel.sendToQueue(queueName, Buffer.from(JSON.stringify(data)));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = {
+    pushToQueue,
+    returnChannel,
+    connectToChannel
+}
