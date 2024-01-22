622beeb2ab1e874461f0ddb5418198d04d3ab4ba
abolfazlrezazadeh <104682369+abolfazlrezazadeh@users.noreply.github.com>
2024-01-22 21:39:55 +0330

    start order server

--- order-service/index.js
+++ order-service/index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
-const {PORT} = process.env;
+const { PORT } = process.env;
 const express = require("express");
-const { orderRoutes } = require("./handler/order.routes");
+const {orderRoutes} = require("./handler/order.routes");
 const app = express();
+// =========== connect to database
+require("./config/db").connectToMongo();
+// config app
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // router
@@ -18,3 +21,6 @@ app.use((error, req, res, next) => {
   return res.json({ error: error.message });
 });
 
+app.listen(PORT, () => {
+  console.log(`order service running on port ${PORT}`);
+});
